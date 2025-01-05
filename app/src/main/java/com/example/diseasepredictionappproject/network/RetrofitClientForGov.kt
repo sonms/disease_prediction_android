9f3aed8e70618cfe845e1da36bbbc3ed1a0beac4
sonms <smsms5676@naver.com>
2025-01-05 23:10:48 +0900

    feat/공공데이터포털 api연동 준비

---
+++ app/src/main/java/com/example/diseasepredictionappproject/network/RetrofitClientForGov.kt
@@ -0,0 +1,28 @@
+package com.example.diseasepredictionappproject.network
+
+import com.example.diseasepredictionappproject.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClientForGov {
+    private const val BASE_URL = BuildConfig.BASE_URL// Emulator의 로컬 서버 주소
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val instance: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
