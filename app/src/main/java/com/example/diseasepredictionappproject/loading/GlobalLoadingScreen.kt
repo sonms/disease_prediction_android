2936ffa9d9cd75ef267c6e17276af8304f4bbe75
sonms <smsms5676@naver.com>
2025-01-11 11:02:22 +0900

    fix/CircleIndicator 호환성 오류 해결

--- app/src/main/java/com/example/diseasepredictionappproject/loading/GlobalLoadingScreen.kt
+++ app/src/main/java/com/example/diseasepredictionappproject/loading/GlobalLoadingScreen.kt
@@ -1,17 +1,25 @@
 package com.example.diseasepredictionappproject.loading
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.example.diseasepredictionappproject.ui.theme.blueColor6
 
 @Composable
 fun GlobalLoadingScreen() {
     val isLoading = LoadingState.isLoading.collectAsState().value
 
     if (isLoading) {
-        Dialog(
+        /*Dialog(
             onDismissRequest = { LoadingState.hide() },
             properties = DialogProperties(
                 dismissOnBackPress = true,
@@ -19,6 +27,17 @@ fun GlobalLoadingScreen() {
             )
         ) {
             CircularProgressIndicator()
+        }*/
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.5f)),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                color = blueColor6,
+                strokeWidth = 8.dp
+            )
         }
     }
 }
