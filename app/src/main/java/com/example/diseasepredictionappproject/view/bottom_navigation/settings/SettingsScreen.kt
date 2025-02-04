dd97b1201035fd4deb8e9c362bdfe28159c77c9c
sonms <smsms5676@naver.com>
2025-02-04 23:16:33 +0900

    feat/setting 화면 UI 수정 및 문의하기 추가

--- app/src/main/java/com/example/diseasepredictionappproject/view/bottom_navigation/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/diseasepredictionappproject/view/bottom_navigation/settings/SettingsScreen.kt
@@ -1,15 +1,21 @@
 package com.example.diseasepredictionappproject.view.bottom_navigation.settings
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -50,32 +56,42 @@ fun SettingsScreen(
 
     //글자 크기 조정?
     val context = LocalContext.current
-    val scope = rememberCoroutineScope()
     val fontSize by PreferenceDataStore.getFontSizeFlow(context).collectAsState(initial = FontSize.Medium)
 
 
 
 
-    Column (
+    LazyColumn (
         modifier = Modifier
             .padding(10.dp)
-            .clickable {
-                isOpen = true
-            },
-    ) {
-        Text(
-            text = "글자 크기 조정",
-            style = FontUtils.getTextStyle(fontSize.size),
-            fontWeight = FontWeight.Bold
-        )
 
-        Text(
-            text = "이곳을 눌러 글자 크기 조절하기",
-            style = FontUtils.getTextStyle(fontSize.size),
-            modifier = Modifier
-                .padding(top = 5.dp),
-            fontWeight = FontWeight.SemiBold
-        )
+    ) {
+        item {
+            SettingDivideItem(mainText = "글자 크기 조정", subText = "이곳을 눌러 글자 크기 조절하기", fontSize = fontSize) {
+                isOpen = !isOpen
+            }
+        }
+
+        item {
+            SettingDivideItem(mainText = "문의하기", subText = "Gmail로 이메일 보내기", fontSize = fontSize) {
+                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:")
+                    putExtra(Intent.EXTRA_EMAIL, arrayOf("smsdia412@gmail.com")) // 받는 사람
+                    putExtra(Intent.EXTRA_SUBJECT, "문의 사항")
+                }
+
+                // Gmail 앱이 설치되어 있는 경우 우선 열기
+                intent.setPackage("com.google.android.gm")
+
+                if (intent.resolveActivity(context.packageManager) != null) {
+                    context.startActivity(intent)
+                } else {
+                    // Gmail이 설치되지 않은 경우 다른 이메일 앱으로 열기
+                    val chooser = Intent.createChooser(intent, "이메일 앱을 선택하세요")
+                    context.startActivity(chooser)
+                }
+            }
+        }
     }
 
     if (isOpen) {
@@ -129,21 +145,27 @@ fun FontSizeSettingDialog(
             shape = RoundedCornerShape(8.dp),
         ) {
             Text(
-                modifier = Modifier.padding(15.dp).clickable { onConfirmClick("작게") },
+                modifier = Modifier
+                    .padding(15.dp)
+                    .clickable { onConfirmClick("작게") },
                 text = "작게",
                 fontWeight = FontWeight.SemiBold,
                 style = FontUtils.getTextStyle(fontSize.size)
             )
 
             Text(
-                modifier = Modifier.padding(15.dp).clickable { onConfirmClick("중간") },
+                modifier = Modifier
+                    .padding(15.dp)
+                    .clickable { onConfirmClick("중간") },
                 text = "중간",
                 fontWeight = FontWeight.SemiBold,
                 style = FontUtils.getTextStyle(fontSize.size + 2f)
             )
 
             Text(
-                modifier = Modifier.padding(15.dp).clickable { onConfirmClick("크게") },
+                modifier = Modifier
+                    .padding(15.dp)
+                    .clickable { onConfirmClick("크게") },
                 text = "크게",
                 fontWeight = FontWeight.SemiBold,
                 style = FontUtils.getTextStyle(fontSize.size + 4f)
@@ -204,5 +226,68 @@ fun DeleteItemDialog(
                 }
             }
         }
+    }
+}
+
+@Composable
+fun SettingDivideItem(
+    mainText : String,
+    subText : String,
+    fontSize : FontSize,
+    onClickItem : () -> Unit
+) {
+    when(mainText) {
+        "문의하기" -> {
+            Column (
+                modifier = Modifier.clickable { onClickItem() }
+            ) {
+                Text(
+                    text = mainText,
+                    style = FontUtils.getTextStyle(fontSize.size),
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = subText,
+                    style = FontUtils.getTextStyle(fontSize.size-2f),
+                    modifier = Modifier
+                        .padding(top = 5.dp),
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                HorizontalDivider(
+                    modifier = Modifier.fillMaxWidth().padding(top = 10.dp),
+                    thickness = 2.dp,
+                    color = Color.Black.copy(0.5f)
+                )
+            }
+        }
+
+        else -> {
+            Column (
+                modifier = Modifier.clickable { onClickItem() }
+            ) {
+                Text(
+                    text = mainText,
+                    style = FontUtils.getTextStyle(fontSize.size),
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = subText,
+                    style = FontUtils.getTextStyle(fontSize.size-2f),
+                    modifier = Modifier
+                        .padding(top = 5.dp),
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                HorizontalDivider(
+                    modifier = Modifier.fillMaxWidth().padding(top = 10.dp),
+                    thickness = 2.dp,
+                    color = Color.Black.copy(0.5f)
+                )
+            }
+        }
+
     }
 }
