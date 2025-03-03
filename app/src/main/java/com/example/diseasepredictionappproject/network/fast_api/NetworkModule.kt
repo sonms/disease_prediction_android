1aa7cf855c548332347bff4bed59bc3bdd7e08c0
sonms <smsms5676@naver.com>
2025-03-03 18:07:10 +0900

    chore/타임아웃 설정

--- app/src/main/java/com/example/diseasepredictionappproject/network/fast_api/NetworkModule.kt
+++ app/src/main/java/com/example/diseasepredictionappproject/network/fast_api/NetworkModule.kt
@@ -6,8 +6,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -20,8 +22,15 @@ object NetworkModule {
     @Singleton
     @BaseUrlRetrofit // Qualifier 추가
     fun provideRetrofit(): Retrofit {
+        val client = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
